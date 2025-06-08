65dcfb7755e232438ee625b60bbcee53420ffab8
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-06-08 15:17:47 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,45 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <memory>
+
+// Узел дерева
+struct Node {
+    char value;
+    std::vector<std::shared_ptr<Node>> children;
+
+    explicit Node(char val);
+};
+
+// Класс дерева перестановок
+class PMTree {
+ private:
+    std::shared_ptr<Node> root;
+    int permutation_count_;
+
+    void BuildSubtree(std::shared_ptr<Node> node,
+        const std::vector<char>& remaining);
+
+ public:
+    explicit PMTree(const std::vector<char>& symbols);
+
+    std::shared_ptr<Node> GetRoot() const;
+    int GetPermutationCount() const;
+
+    static void CollectPermutations(const std::shared_ptr<Node>& node,
+        std::vector<char>& path,
+        std::vector<std::vector<char>>& result);
+    friend void CountPermutationsImpl(const std::shared_ptr<Node>& node,
+        int& count);
+
+    friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+    friend std::vector<char> getPerm1(PMTree& tree, int num);
+    friend std::vector<char> getPerm2(PMTree& tree, int num);
+};
+
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
 #endif  // INCLUDE_TREE_H_
