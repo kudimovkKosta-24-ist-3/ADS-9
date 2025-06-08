be962428e2bd83dab9558c19aa33c4328124133a
amidamAru <ivanadrei18@gmail.com>
2025-06-08 12:45:40 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,46 +1,47 @@
-// Copyright 2022 NNTU-CS
-#ifndef TREE_H
-#define TREE_H
+#ifndef INCLUDE_TREE_H_
+#define INCLUDE_TREE_H_
 
 #include <vector>
 #include <memory>
 
+// Узел дерева
 struct Node {
-  char value;
-  std::vector<std::shared_ptr<Node>> children;
+    char value;
+    std::vector<std::shared_ptr<Node>> children;
 
-  explicit Node(char val);
+    explicit Node(char val);
 };
 
+// Класс дерева перестановок
 class PMTree {
- private:
-  std::shared_ptr<Node> root;
-  int permutation_count_;
+private:
+    std::shared_ptr<Node> root;
+    int permutation_count_;
 
-  void BuildSubtree(std::shared_ptr<Node> node,
-                    const std::vector<char>& remaining);
-  void CountPermutations(std::shared_ptr<Node> node, int& count) const;
+    void BuildSubtree(std::shared_ptr<Node> node,
+        const std::vector<char>& remaining);
 
- public:
-  explicit PMTree(const std::vector<char>& symbols);
+public:
+    explicit PMTree(const std::vector<char>& symbols);
 
-  std::shared_ptr<Node> GetRoot() const;
-  int GetPermutationCount() const;
+    std::shared_ptr<Node> GetRoot() const;
+    int GetPermutationCount() const;
 
-  static void CollectPermutations(const std::shared_ptr<Node>& node,
-                                  std::vector<char>& path,
-                                  std::vector<std::vector<char>>& result);
+    static void CollectPermutations(const std::shared_ptr<Node>& node,
+        std::vector<char>& path,
+        std::vector<std::vector<char>>& result);
 
-  friend bool FindNthPermutation(PMTree& tree,
-                                 const std::shared_ptr<Node>& node,
-                                 std::vector<char>& path,
-                                 std::vector<char>& result,
-                                 int& current_num,
-                                 int target_num);
+    friend void CountPermutationsImpl(const std::shared_ptr<Node>& node, 
+        int& count);
+
+    friend std::vector<std::vector<char>> GetAllPerms(PMTree& tree);
+    friend std::vector<char> GetPerm1(PMTree& tree, int num);
+    friend std::vector<char> GetPerm2(PMTree& tree, int num);
 };
 
+// Функции для работы с перестановками
 std::vector<std::vector<char>> GetAllPerms(PMTree& tree);
-std::vector<char> GetPerm1(PMTree& tree, int num);
-std::vector<char> GetPerm2(PMTree& tree, int num);
+std::vector<char> GetPerm1(PMTree& tree, int num);  // через предварительный обход
+std::vector<char> GetPerm2(PMTree& tree, int num);  // через навигацию по дереву
 
-#endif  // TREE_H
+#endif  // INCLUDE_TREE_H_
