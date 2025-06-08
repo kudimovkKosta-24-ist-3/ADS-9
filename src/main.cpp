a0495042d03d6ee4b1317b327d76dd84850fa564
amidamAru <ivanadrei18@gmail.com>
2025-06-08 12:48:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,33 @@
 // Copyright 2022 NNTU-CS
-#include "tree.h"
-
 #include <iostream>
 #include <vector>
 #include <chrono>
 #include <random>
+#include "tree.h"
 
 // Функции измерения времени
-long long MeasureGetAllPerms(PMTree& tree) {
+uint16_t MeasureGetAllPerms(PMTree& tree) {
     auto start = std::chrono::high_resolution_clock::now();
     GetAllPerms(tree);
     auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>
+        (end - start).count();
 }
 
-long long MeasureGetPerm1(PMTree& tree, int num) {
+uint16_t MeasureGetPerm1(PMTree& tree, int num) {
     auto start = std::chrono::high_resolution_clock::now();
     GetPerm1(tree, num);
     auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>
+        (end - start).count();
 }
 
-long long MeasureGetPerm2(PMTree& tree, int num) {
+uint16_t MeasureGetPerm2(PMTree& tree, int num) {
     auto start = std::chrono::high_resolution_clock::now();
     GetPerm2(tree, num);
     auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>
+        (end - start).count();
 }
 
 int main() {
@@ -44,11 +46,12 @@ int main() {
         std::uniform_int_distribution<int> dist(1, total_perms);
         int random_num = dist(generator);
 
-        long long time_all = MeasureGetAllPerms(tree);
-        long long time_1 = MeasureGetPerm1(tree, random_num);
-        long long time_2 = MeasureGetPerm2(tree, random_num);
+        uint16_t time_all = MeasureGetAllPerms(tree);
+        uint16_t time_1 = MeasureGetPerm1(tree, random_num);
+        uint16_t time_2 = MeasureGetPerm2(tree, random_num);
 
-        std::cout << n << "\t" << time_all << "\t" << time_1 << "\t" << time_2 << "\n";
+        std::cout << n << "\t" << time_all << "\t" << time_1 << "\t" 
+            << time_2 << "\n";
     }
 
     return 0;
